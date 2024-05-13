16cddca9a5453a68aa67be85bc1722b41643acd0
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2024-05-13 13:14:21 -0400

    Update cardsService.js

--- src/services/cardsService.js
+++ src/services/cardsService.js
@@ -1,3 +1,40 @@
-const cardsData = {}
+const cardsData = {
+    "painelDiarias": {
+        cardImage: {
+            url1:'../assets/cards BI - ativos/painel diarias - ativo.jpg',
+            alt1:'Painel diárias - banner - ativo',
+            url2:'../assets/cards BI - inativos/painel diarias - inativo.jpg',
+            alt2:'Painel diárias - banner - inativo '
+        },
+       linkURL: '#'
+    },
+    "painelObras": {
+        cardImage: {
+            url1:'../assets/cards BI - ativos/painel obras - ativo.jpg',
+            alt1:'Painel obras - banner - ativo',
+            url2:'../assets/cards BI - inativos/painel obras - inativo.jpg',
+            alt2:'Painel obras - banner - inativo '
+        },
+        linkURL: '#'
+    },
+    "painelEPonto": {
+        cardImage: {
+            url1:'../assets/cards BI - ativos/painel e-ponto - ativo.jpg',
+            alt1:'Painel e-ponto - banner - ativo',
+            url2:'../assets/cards BI - inativos/painel e-ponto - inativo.jpg',
+            alt2:'Painel e-ponto - banner - inativo '
+        },
+        linkURL: '#'
+    },
+    "painelSupFundo": {
+        cardImage: {
+            url1:'../assets/cards BI - ativos/painel sup fundo - ativo.jpg',
+            alt1:'Painel suprimento de fundo - banner - ativo',
+            url2:'../assets/cards BI - inativos/painel sup fundo - inativo.jpg',
+            alt2:'Painel suprimento de fundo - banner - inativo '
+        },
+        linkURL: '#'
+    },
+}
 
 export default cardsData;
